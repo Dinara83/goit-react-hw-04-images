bee09fe7abfc6b95a6ed90c76ab5a283f29bb6d7
Dinara83 <mavleeva.dinara@gmail.com>
2023-02-05 09:11:55 +0200

    css loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,18 @@
 import { Dna } from 'react-loader-spinner';
 
+import css from './loader.module.css';
+
 export const Loader = () => {
   return (
-    <>
+    <div className={css.overlay}>
       <Dna
         visible={true}
-        height="80"
-        width="80"
+        height="180"
+        width="180"
         ariaLabel="dna-loading"
         wrapperStyle={{ display: 'block', margin: 'auto' }}
         wrapperClass="dna-wrapper"
       />
-    </>
+    </div>
   );
 };
